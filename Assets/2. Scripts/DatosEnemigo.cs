e6fbb2876e8741bcc99f6edc357b4402335537e7
Aleix <aleixhedzbs@gmail.com>
2026-01-11 11:07:18 +0100

    commit de mierda
    caca caca

---
+++ Assets/2. Scripts/DatosEnemigo.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NuevoEnemigo", menuName = "Juego/Datos Enemigo")]
+public class DatosEnemigo : ScriptableObject
+{
+    public string nombreEnemigo;
+    public Sprite spriteEnemigo;
+}
---
+++ Assets/2. Scripts/UnidadEnemiga.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class UnidadEnemiga : MonoBehaviour
+{
+    // Aquí irá la lógica de salud o animaciones del rival
+}
